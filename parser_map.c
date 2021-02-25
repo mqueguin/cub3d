dfca4e8df37bfe78fe247781cc30defe3e2a8246
Martinq76 <martin.queguiner.42@gmail.com>
2021-02-25 23:11:10 +0100

    M

--- parser_map.c
+++ parser_map.c
@@ -6,10 +6,15 @@
 /*   By: mqueguin <mqueguin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 22:53:54 by mqueguin          #+#    #+#             */
-/*   Updated: 2021/02/25 22:54:31 by mqueguin         ###   ########.fr       */
+/*   Updated: 2021/02/25 23:10:55 by mqueguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /** Fonction qui permet de vérifier si une map est valid **/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
 
 

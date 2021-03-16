a2437298c08bd9a08dc4535f5ea172e476232e52
mqueguin <mqueguin@student.42.fr>
2021-03-16 10:51:45 +0100

    Corrections ft_parse aui marchait plus a cause des espaces

--- ft_parse.c
+++ ft_parse.c
@@ -6,7 +6,7 @@
 /*   By: mqueguin <mqueguin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 11:28:28 by mqueguin          #+#    #+#             */
-/*   Updated: 2021/03/12 15:04:03 by mqueguin         ###   ########.fr       */
+/*   Updated: 2021/03/16 10:50:49 by mqueguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,20 @@ int	ft_parse_line(t_info_game *info_game, char *line)
 	info_game->line_index += 1;
 	ft_bzero(info_game->parse_char, 3);
 	if (line[0] == '\0')
+	{
+		printf("Hello\n");
 		return (1);
+	}
+	if (line[i] == ' ')
+	{
+		i = ft_jump_space(line, i);
+		if (line[i] == '\0')
+			return (1);
+			else return (0);
+	}
 	if (line[i] == 'R')
 	{
+		printf("Coucou\n");
 		if (!(ft_parse_res(info_game, line, i)))
 			return (0);
 	}

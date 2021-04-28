dde6bd46bf5f94e5cba1df81660273cebc8ae0f9
mqueguin <mqueguin@student.42.fr>
2021-04-28 17:49:11 +0200

    Affichages des textures OK :D

--- srcs/ft_textures.c
+++ srcs/ft_textures.c
@@ -6,13 +6,13 @@
 /*   By: mqueguin <mqueguin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 23:51:20 by mqueguin          #+#    #+#             */
-/*   Updated: 2021/04/26 16:50:42 by mqueguin         ###   ########.fr       */
+/*   Updated: 2021/04/28 17:47:12 by mqueguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-static	int			print_tex_wall(t_game *game, int side, int y, int x)
+static	int			print_tex_wall(t_game *game, t_textures textures, int side, int y, int x)
 {
 	float			tex_y;
 	unsigned	int	color;
@@ -23,9 +23,9 @@ static	int			print_tex_wall(t_game *game, int side, int y, int x)
 	{
 		tex_y = (int)(((y - game->info_game.win_res[1] /
 			2 + game->rays.line_height / 2) *
-			game->north_text.height) / game->rays.line_height);
-		color = game->north_text.tex[game->north_text.width *
-			(int)floor(tex_y) + game->north_text.texture_x];
+			textures.height) / game->rays.line_height);
+		color = textures.tex[textures.width *
+			(int)floor(tex_y) + textures.texture_x];
 		my_mlx_pixel_put(game, x, y, color);
 		++y;
 	}
@@ -54,7 +54,7 @@ int					wich_plan(t_game *game)
 	return (side);
 }
 
-static	int			tex_x_calcul(t_game *game, int side)
+static	int			tex_x_calcul(t_game *game, t_textures textures, int side)
 {
 	int		tex_x;
 	float	wall_x;
@@ -68,42 +68,55 @@ static	int			tex_x_calcul(t_game *game, int side)
 		wall_x = game->player.pos_x + game->rays.perp_wall_dist *
 			game->rays.ray_dir_x;
 	wall_x = wall_x - floor(wall_x);
-	tex_x = (int)(wall_x * (float)game->north_text.width);
+	tex_x = (int)(wall_x * (float)textures.width);
 	if (side == 0)
-		tex_x = game->north_text.width - tex_x - 1;
+		tex_x = textures.width - tex_x - 1;
 	if (side == 3)
-		tex_x = game->north_text.width - tex_x - 1;
+		tex_x = textures.width - tex_x - 1;
 	return (tex_x);
 }
 
 void	ft_print_textures(t_game *game, int x, int draw_start, int draw_end)
 {
 	int		y;
-	int		color_c;
-	int		color_f;
+	int		colors[2];
 	int		side;
 	int		text_y;
 
 	y = 0;
 	side = wich_plan(game);
-	game->north_text.texture_x = tex_x_calcul(game, side);
-	color_c = ft_convert_to_trgb(0, game->info_game.color_c[0], game->info_game.color_c[1]
-				, game->info_game.color_c[2]);
-	color_f = ft_convert_to_trgb(0, game->info_game.color_f[0], game->info_game.color_f[1]
-				, game->info_game.color_f[2]);
+	if (side == 0)
+		game->north_text.texture_x = tex_x_calcul(game, game->north_text, side);
+	else if (side == 1)
+		game->south_text.texture_x = tex_x_calcul(game, game->south_text, side);
+	else if (side == 2)
+		game->west_text.texture_x = tex_x_calcul(game, game->west_text, side);
+	else if (side == 3)
+		game->east_text.texture_x = tex_x_calcul(game, game->east_text, side);
+	colors[0] = ft_convert_to_trgb(0, game->info_game.color_c[0], game->info_game.color_c[1],
+				game->info_game.color_c[2]);
+	colors[1] = ft_convert_to_trgb(0, game->info_game.color_f[0], game->info_game.color_f[1],
+				game->info_game.color_f[2]);
 	text_y = 0;
 	while (y < draw_start)
 	{
-		my_mlx_pixel_put(game, x, y, color_c);
+		my_mlx_pixel_put(game, x, y, colors[0]);
 		y++;
 	}
 	//Print des textures du mur A FAIRE
-	y = print_tex_wall(game, side, y, x);
+	if (side == 0)
+		y = print_tex_wall(game, game->north_text, side, y, x);
+	else if (side == 1)
+		y = print_tex_wall(game, game->south_text, side, y, x);
+	else if (side == 2)
+		y = print_tex_wall(game, game->west_text, side, y, x);
+	else if (side == 3)
+		y = print_tex_wall(game, game->east_text, side, y, x);
 	//printf("valeur de y : %d\n", y);
 	//printf("valeur de res_y : %d\n", game->info_game.win_res[1]);
 	while (draw_end <= y && y < game->info_game.win_res[1])
 	{
-		my_mlx_pixel_put(game, x, y, color_f);
+		my_mlx_pixel_put(game, x, y, colors[1]);
 		y++;
 	}
 
